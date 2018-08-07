2e29b57185f151451d09aa9e3168aa9bef038b8e
Patrick <sqllensman@outlook.com>
2018-08-07 15:41:05 +1000

    Updated Script

--- SQLSaturday SriLanka 750/GitHub/Dbatools_Fix.ps1
+++ SQLSaturday SriLanka 750/GitHub/Dbatools_Fix.ps1
@@ -2,7 +2,14 @@
 Import-Module 'C:\GitHub\dbatools\dbatools.psd1'
 
 
-Get-DbaAgentJob -SqlInstance LensmanSB | Export-DbaScript -path  "C:\Lensman\Output\script4.sql" -BatchSeparator "GO" -NoPrefix
+$options = New-DbaScriptingOption
+$options.NoCommandTerminator  = $false
+$options.IncludeDatabaseContext  = $true
+$options.IncludeHeaders = $true
+$Options.ScriptBatchTerminator = $true 
+$Options.AnsiFile = $true
+
+Get-DbaAgentJob -SqlInstance LensmanSB | Export-DbaScript -path  "C:\Lensman\Output\script4.sql" -NoPrefix -ScriptingOptionsObject $Options
 
 
 Get-DbaAgentJob -SqlInstance LensmanSB | Export-DbaScript -Passthru | ForEach-Object { $_ } | Out-GridView #  Set-Content -Path "C:\Lensman\Output\script1.sql"
