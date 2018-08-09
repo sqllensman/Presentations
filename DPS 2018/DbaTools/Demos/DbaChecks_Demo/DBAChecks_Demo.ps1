73163c1e9df8a2061d1aa0d9b58385b900594e4b
Patrick <sqllensman@outlook.com>
2018-08-09 21:32:26 +1000

    Demo Code for Presentation

---
+++ DPS 2018/DbaTools/Demos/DbaChecks_Demo/DBAChecks_Demo.ps1
@@ -0,0 +1,247 @@
+﻿Return 'This is a demo, don''t run the whole thing, fool!!'
+<#
+    Created June 2018
+    Using dbchecks Version 1.1.138
+
+#>
+
+#region Module Information
+
+Import-Module -Name dbachecks
+
+# Check Versions of Dependant Modules
+Get-Module -Name dbatools | Select Path, Version | Out-GridView
+Get-Module -Name PSFramework | Select Path, Version | Out-GridView
+Get-Module -Name Pester | Select Path, Version | Out-GridView
+
+# Check Version and Location
+Get-Module -Name dbachecks | Select Path, Version | Out-GridView
+
+# List Available Commands (17)
+Get-Command -Module dbachecks -CommandType Function | Out-GridView
+
+#endregion
+
+
+# Just Backups
+$servers = 'WIN-2012-SQL01\SQL2008','WIN-2012-SQL01\SQL2008R2','WIN-2012-SQL01\SQL2012','WIN-2012-SQL01\SQL2014','W2016BASE','W2016BASE\SQL2017'
+Invoke-DbcCheck -SqlInstance $servers -Checks LastBackup -Show Fails, Summary 
+
+Get-DbcConfig -Name *backup*
+
+
+#region List of Available Checks and Tags (look at Config  later)
+# Get List of Checks
+$Checks = Get-DbcCheck
+
+#How Many (80 on first release) - 91 for 1.13
+$Checks.Count
+
+#View Available
+$Checks  | Out-GridView
+
+# Tags
+$Tags = Get-DbcTagCollection
+$Tags.Count
+
+$Tags | Out-GridView
+
+#endregion
+
+#region Running With Invoke-DbcCheck	
+
+# View Help for Invoke-DbcCheck
+Get-Help Invoke-DbcCheck -ShowWindow
+
+
+#  Run the Checks for Single Server and Particular Checks
+$servers = 'W2016Base'
+Invoke-DbcCheck -SqlInstance $servers -Checks SuspectPage, IdentityUsage, PseudoSimple 
+
+
+#  Run the Checks for Single Server and Particular Checks - Only Output 
+Invoke-DbcCheck -SqlInstance $servers  -Checks SuspectPage, IdentityUsage, PseudoSimple -Show Fails
+
+Invoke-DbcCheck -ComputerName $servers  -Checks PowerPlan -Show Fails
+
+
+# Run Checks with Tag Instance
+Invoke-DbcCheck -SqlInstance $servers -Checks Instance 
+
+# Run Checks with Tag Instance and Save output Does not display output
+# Output written to 
+Invoke-DbcCheck -SqlInstance $servers -Checks Instance -Show Fails, Summary
+
+
+$Results = Invoke-DbcCheck -SqlInstance $servers -Checks Instance, Identity -Show Summary -PassThru 
+
+$Results.GetType()
+
+# View Output
+$Results.FailedCount
+$Results.InconclusiveCount
+$Results.PassedCount
+$Results.PendingCount
+$ResultS.PassedCount
+$ResultS.SkippedCount
+$Results.TotalCount
+
+$Results.ExcludeTagFilter
+$Results.TagFilter
+$Results.Time
+
+
+$Results.TestNameFilter
+
+$Results.TestResult | Select Describe, Context, Name, Passed, Result, FailureMessage, StackTrace, Time | Out-GridView
+
+# Can filter to see skipped or failed tests
+
+# Skipped Results - What where they
+$Results.TestResult | Where Result -eq 'Skipped' | Out-GridView
+
+# Why Skipped ?
+Get-DbcConfigValue -Name skip.tempdbfilecount
+#Set-DbcConfig -Name skip.tempdbfilecount -Value $false
+
+
+
+
+# Save to JSON file needed by PowerBI
+Clear-DbcPowerBiDataSource 
+$Results | Update-DbcPowerBiDataSource -Environment LocalHostTest
+
+
+
+#View report
+Start-DbcPowerBi
+
+<#
+
+    Basically does this:
+    $InputObject.TestResult | Select-Object -First 20 | ConvertTo-Json -Depth 3 | Out-File "$env:windir\temp\dbachecks.json"
+
+    oUTPUT IN $env:windir\temp\dbachecks.json
+
+    Get-Help Update-DbcPowerBiDataSource -ShowWindow
+#>
+
+#Can be run as:
+Invoke-DbcCheck -SqlInstance $servers -Checks Instance, Identity -Show Summary -PassThru | Update-DbcPowerBiDataSource -Environment LocalHostTest
+Start-DbcPowerBi
+
+
+#endregion
+
+
+#region Configuration
+
+<#
+dbachecks enables you to set configuration items at run-time or for your session 
+Configurations can be exported and imported so you can create different configs for different use cases
+
+Configurations are persisted via Registry Key
+
+There are 150 (June 2018) configuration items at present. You can see the current configuration by running
+
+#>
+
+#Configurations
+$Config = Get-DbcConfig
+
+$Config.Count
+
+$Config | Select Name , Description, Value | Out-GridView
+
+# Set the servers you'll be working with
+Set-DbcConfig -Name app.computername -Value WIN-2012-SQL01, W2016BASE
+Set-DbcConfig -Name app.sqlinstance -Value W2016BASE\SQL2017, W2016BASE, WIN-2012-SQL01\SQL2014
+Set-DbcConfig -Name policy.whoisactive.database -Value DBAAdmin 
+
+Get-DbcConfigValue -Name app.computername
+Get-DbcConfigValue -Name app.sqlinstance
+
+
+
+# Invoke a few tests
+Invoke-DbcCheck -Checks Instance, Server
+
+
+
+# Check for WhoIsActive failed
+# Check current configuration
+Get-DbcConfigValue -Name policy.whoisactive.database 
+
+
+Set-DbcConfig -Name policy.whoisactive.database -Value master
+
+Invoke-DbcCheck -Checks WhoIsActiveInstalled
+
+Export-DbcConfig -Path C:\SQLSaturday\Melbourne\Config\test_config.json
+
+#Reset Configs to Default
+Reset-DbcConfig
+
+#Try running Check
+Invoke-DbcCheck -Checks WhoIsActiveInstalled
+
+#Import Config from File
+Import-DbcConfig -Path C:\SQLSaturday\Melbourne\Config\test_config.json
+
+#endregion
+
+#region Using a Registered Server
+# Clear PowerBi Data first
+Clear-DbcPowerBiDataSource
+
+$cmsServer = 'W2016Base'
+$groupName = 'Production'
+$sqlinstances = Get-DbaRegisteredServer -SqlInstance $cmsServer -Group $groupName
+
+$sqlinstances | Out-GridView
+
+$tags = 'AutoClose', 'AutoShrink', 'DAC', 'TempDbConfiguration', 'DatafileAutoGrowthType'
+
+$tags | ForEach-Object {
+    $tag = $PSItem
+ 
+    $obj =  [pscustomobject]@{            
+        ServerGroup = $groupName
+        NumServers = $sqlinstances.Count
+        Tag = $tag
+        InvokeStartTime = Get-Date
+        InvokeCompleteTime = $null
+        WriteResultsTime = $null
+        TestExecution = $null
+        InvokeDuration = $null
+        ResultsDuration = $null
+        PassedCount       = $null
+        FailedCount       = $null
+        SkippedCount      = $null
+        PendingCount      = $null
+        InconclusiveCount = $null               
+    }
+             
+    $results = Invoke-DbcCheck -SqlInstance $sqlinstances -tags $tag -PassThru -Show Fails
+    $obj.TestExecution     = $results.time
+    $obj.PassedCount       = $results.PassedCount      
+    $obj.FailedCount       = $results.FailedCount      
+    $obj.SkippedCount      = $results.SkippedCount     
+    $obj.PendingCount      = $results.PendingCount     
+    $obj.InconclusiveCount = $results.InconclusiveCount
+ 
+    $obj.InvokeCompleteTime = Get-Date
+ 
+    $results | Update-DbcPowerBiDataSource -Environment $groupName -Append
+    $obj.WriteResultsTime = Get-Date      
+ 
+    $obj.InvokeDuration = New-TimeSpan -Start $obj.InvokeStartTime -End $obj.InvokeCompleteTime
+    $obj.ResultsDuration = New-TimeSpan -Start $obj.InvokeCompleteTime -End $obj.WriteResultsTime 
+      
+} # $tags | ForEach-Object 
+
+
+Start-DbcPowerBi
+
+
+#endregion              
--- DPS 2018/DbaTools/Demos/ScriptDatabase.ps1
+++ DPS 2018/DbaTools/Demos/ScriptDatabase.ps1
@@ -1,12 +1,13 @@
 ﻿#Return 'This is a demo, don''t run the whole thing, fool!!'
 #Remove-Module dbatools
-
-
+Import-Module -Name 'C:\GitHub\dbatools\dbatools.psd1'
 
 
 $server = 'LensmanSB'
+$DatabaseName = 'pubs'
 $BasePath = 'C:\Lensman\Output\DatabaseScripts'
 $Dir = $BasePath +'\' + $server #+ $(get-date -f yyyy-MM-dd_HH_mm_ss)
+
 
 # Create Directory if Required
 if(!(Test-Path -Path $Dir )){
@@ -16,107 +17,403 @@ if(!(Test-Path -Path $Dir )){
 #Set Scripting Options
 $options = New-DbaScriptingOption    
 $options.ScriptSchema = $true
+$options.IncludeDatabaseContext  = $true
+$options.IncludeHeaders = $false
+$Options.NoCommandTerminator = $false
+$Options.ScriptBatchTerminator = $true 
+$Options.AnsiFile = $true
 
-$Databases = Get-DbaDatabase -SqlInstance $server -Database WideWorldImporters   #-ExcludeDatabase master, model, msdb,tempdb
+$Databases = Get-DbaDatabase -SqlInstance $server -Database $DatabaseName   #-ExcludeDatabase master, model, msdb,tempdb
 
 foreach ($db in $Databases) {
-    #region Script out Database Create Scripts
-    
     $DirDatabase = $Dir + '\' + $db.Name
+    #region Database Scripts
+    
     # Build The Required Directories
     if(!(Test-Path -Path $DirDatabase)){
         # Create Directory for Database
         New-Item -ItemType directory -Path $DirDatabase
     }
-
-    # Create Database Scripts
     $FileName = $DirDatabase+ '\' + $db.Name + '.sql'
-    Export-DbaScript -InputObject $db -Path $FileName -Encoding UTF8 -ScriptingOptionsObject $options
+
+    # Generate Scripts    
+    Export-DbaScript -InputObject $db -Path $FileName -Encoding UTF8 -ScriptingOptionsObject $options -NoPrefix
     #endregion
 
-    #region Script out Schema Creation
-    $Schemas = $db.Schemas
-    $currentPath = $DirDatabase + '\Schemas'
-    if(!(Test-Path -Path $currentPath)){
-        # Create Directory for Database
-        New-Item -ItemType directory -Path $currentPath
-    }
+    #region Schema Creation
+    $Schemas = ($db.Schemas | Where-Object {$_.IsSystemObject -eq $false})
+    if ($Schemas.Count -gt 0) {
+        #Scripting Options
+        $options.IncludeDatabaseContext  = $true
+        $Options.IncludeIfNotExists = $true
 
-    $FileName = $currentPath + '\' + $db.Name + '_Schema.sql'
-    $options.IncludeDatabaseContext  = $true
-    $Options.IncludeIfNotExists = $true
-
-    
-    foreach($Schema in $Schemas) {
-        if (!$Schema.IsSystemObject) {         
-            Export-DbaScript -InputObject $Schema -Path $FileName -ScriptingOptionsObject $options -Append
+        $currentPath = $DirDatabase + '\Schemas'
+        if(!(Test-Path -Path $currentPath)){
+            # Create Directory for Database
+            New-Item -ItemType directory -Path $currentPath
         }
-    } 
+
+        $FileName = $currentPath + '\' + $db.Name + '_Schema.sql'
+
+        # Generate Scripts
+        foreach($Schema in $Schemas) {       
+            Export-DbaScript -InputObject $Schema -Path $FileName -ScriptingOptionsObject $options -Append -NoPrefix
+        } 
+    }
     #endregion
 
-    #region Script out Table Creation
+    #region Table Creation
     $Tables = $db.Tables
+    if ($Tables.Count -gt 0) {
+        #Scripting Options
+        $options.ColumnStoreIndexes = $true
+        $options.ConvertUserDefinedDataTypesToBaseType  = $true
+        $options.DriAll  = $true
+        $options.IncludeDatabaseContext  = $true
+        $Options.IncludeIfNotExists = $false
+        $options.Indexes  = $true
+        $options.NoFileGroup = $false
+        $options.NonClusteredIndexes  = $true
+        $options.ScriptBatchTerminator  = $true
+        $options.SpatialIndexes  = $true
+        $options.XmlIndexes  = $true
 
-    #Table Scripting Options
-    #$options.ClusteredIndexes = $true
-    $options.ColumnStoreIndexes = $true
-    $options.ConvertUserDefinedDataTypesToBaseType  = $true
-    $options.DriAll  = $true
-    $options.IncludeDatabaseContext  = $true
-    $Options.IncludeIfNotExists = $false
-    $options.Indexes  = $true
-    $options.NoFileGroup = $false
-    $options.NonClusteredIndexes  = $true
-    $options.ScriptBatchTerminator  = $true
-    $options.SpatialIndexes  = $true
-    $options.XmlIndexes  = $true
-
-    # Create Directory for Table Scripts
-    $currentPath = $DirDatabase + '\tables'
-    if(!(Test-Path -Path $currentPath)){
-        # Create Directory for Database
-        New-Item -ItemType directory -Path $currentPath
-    }
+        # Create Directory for Scripts
+        $currentPath = $DirDatabase + '\Tables'
+        if(!(Test-Path -Path $currentPath)){
+            # Create Directory for Database
+            New-Item -ItemType directory -Path $currentPath
+        }
     
-    foreach($Table in $Tables) {
-        $FileName = $currentPath + '\' + $Table.Schema + '.' + $Table.Name + '.sql'
-        Export-DbaScript -InputObject $Table -Path $FileName -ScriptingOptionsObject $options
-    }
-    #endregion
-
-    #region Script out View Creation
-    $Views = $db.Views
-
-    #Table Scripting Options
-    $options.ClusteredIndexes = $true
-    $options.ColumnStoreIndexes = $true
-    $options.ConvertUserDefinedDataTypesToBaseType  = $true
-    $options.DriAll  = $true
-    $options.IncludeDatabaseContext  = $true
-    $Options.IncludeIfNotExists = $false
-    $options.Indexes  = $true
-    $options.NoFileGroup = $false
-    $options.NonClusteredIndexes  = $true
-    $options.ScriptBatchTerminator  = $true
-    $options.SpatialIndexes  = $true
-    $options.XmlIndexes  = $true
-
-    # Create Directory for Table Scripts
-    $currentPath = $DirDatabase + '\views'
-    if(!(Test-Path -Path $currentPath)){
-        # Create Directory for Database
-        New-Item -ItemType directory -Path $currentPath
-    }
-    
-    foreach($View in $Views) {
-        if (!$View.IsSystemObject) { 
-            $FileName = $currentPath + '\' + $View.Schema +'.' + $View.Name + '.sql'
-            Export-DbaScript -InputObject $View -Path $FileName -ScriptingOptionsObject $options
+        # Generate Scripts
+        foreach($Table in $Tables) {
+            $FileName = $currentPath + '\' + $Table.Schema + '.' + $Table.Name + '.sql'
+            Export-DbaScript -InputObject $Table -Path $FileName -ScriptingOptionsObject $options -NoPrefix
         }
     }
     #endregion
+
+    #region View Creation
+    $Views = ($db.Views | Where-Object {$_.IsSystemObject -eq $false})
+    if ($Views.Count -gt 0) {
+        #Scripting Options
+        $options.IncludeDatabaseContext  = $true
+        $Options.IncludeIfNotExists = $false
+        $options.Indexes  = $true
+        $options.NoFileGroup = $false
+
+        # Create Directory for Scripts
+        $currentPath = $DirDatabase + '\Views'
+        if(!(Test-Path -Path $currentPath)){
+            # Create Directory for Database
+            New-Item -ItemType directory -Path $currentPath
+        }
+
+        # Generate Scripts
+        foreach($View in $Views) {
+            $FileName = $currentPath + '\' + $View.Schema +'.' + $View.Name + '.sql'
+            Export-DbaScript -InputObject $View -Path $FileName -ScriptingOptionsObject $options -NoPrefix
+        }
+    }
+    #endregion
+    
+    #region Stored Procedure Creation
+    $StoredProcedures = ($db.StoredProcedures | Where-Object {$_.IsSystemObject -eq $false})
+    if ($StoredProcedures.Count -gt 0) {
+        #Scripting Options
+
+        $Options.IncludeIfNotExists = $false
+
+
+        # Create Directory for Scripts
+        $currentPath = $DirDatabase + '\StoredProcedures'
+        if(!(Test-Path -Path $currentPath)){
+            # Create Directory for Database
+            New-Item -ItemType directory -Path $currentPath
+        }
+
+        # Generate Scripts
+        foreach($sp in $StoredProcedures) {
+            $FileName = $currentPath + '\' + $sp.Schema +'.' + $sp.Name + '.sql'
+            Export-DbaScript -InputObject $sp -Path $FileName -ScriptingOptionsObject $options -NoPrefix
+        }
+    }
+    #endregion
+
+    #region UserDefinedFunctions Creation
+    $UserDefinedFunctions = ($db.UserDefinedFunctions | Where-Object {$_.IsSystemObject -eq $false})
+    if ($UserDefinedFunctions.Count -gt 0) {
+        #Scripting Options
+
+        # Create Directory for Scripts
+        $currentPath = $DirDatabase + '\UserDefinedFunctions'
+        if(!(Test-Path -Path $currentPath)){
+            # Create Directory for Database
+            New-Item -ItemType directory -Path $currentPath
+        }
+
+        # Generate Scripts
+        foreach($udf in $UserDefinedFunctions) {
+            $FileName = $currentPath + '\' + $udf.Schema +'.' + $udf.Name + '.sql'
+            Export-DbaScript -InputObject $udf -Path $FileName -ScriptingOptionsObject $options -NoPrefix
+        }
+    }
+    #endregion
+
+    #region UserDefinedDataTypes Creation
+    $UserDefinedDataTypes = $db.UserDefinedDataTypes
+    if ($UserDefinedDataTypes.Count -gt 0) {
+        #Scripting Options
+        $options.IncludeDatabaseContext  = $true
+
+
+        # Create Directory for Scripts
+        $currentPath = $DirDatabase + '\UserDefinedDataTypes'
+        if(!(Test-Path -Path $currentPath)){
+            # Create Directory for Database
+            New-Item -ItemType directory -Path $currentPath
+        }
+
+        # Generate Scripts
+        foreach($uddt in $UserDefinedDataTypes) {
+            $FileName = $currentPath + '\' + $uddt.Schema +'.' + $uddt.Name + '.sql'
+            Export-DbaScript -InputObject $uddt -Path $FileName -ScriptingOptionsObject $options -NoPrefix
+        }
+    }
+    #endregion
+
+    #region UserDefinedTableTypes Creation
+    $UserDefinedTableTypes = $db.UserDefinedTableTypes
+    if ($UserDefinedTableTypes.Count -gt 0) {
+        #Scripting Options
+        $options.IncludeDatabaseContext  = $true
+
+
+        # Create Directory for Scripts
+        $currentPath = $DirDatabase + '\UserDefinedTableTypes'
+        if(!(Test-Path -Path $currentPath)){
+            # Create Directory for Database
+            New-Item -ItemType directory -Path $currentPath
+        }
+
+        # Generate Scripts
+        foreach($udtt in $UserDefinedTableTypes) {
+            $FileName = $currentPath + '\' + $udtt.Schema +'.' + $udtt.Name + '.sql'
+            Export-DbaScript -InputObject $udtt -Path $FileName -ScriptingOptionsObject $options -NoPrefix
+        }
+    }
+    #endregion
+
+    #region PartitionFunctions Creation
+    $PartitionFunctions = $db.PartitionFunctions
+    if ($PartitionFunctions.Count -gt 0) {
+        #Scripting Options
+        $options.IncludeDatabaseContext  = $true
+
+
+        # Create Directory for Scripts
+        $currentPath = $DirDatabase + '\PartitionFunctions'
+        if(!(Test-Path -Path $currentPath)){
+            # Create Directory for Database
+            New-Item -ItemType directory -Path $currentPath
+        }
+
+        # Generate Scripts
+        foreach($PartitionFunction in $PartitionFunctions) {
+            $FileName = $currentPath + '\' + $PartitionFunction.Schema +'.' + $PartitionFunction.Name + '.sql'
+            Export-DbaScript -InputObject $PartitionFunction -Path $FileName -ScriptingOptionsObject $options -NoPrefix
+        }
+    }
+    #endregion
+
+    #region PartitionSchemes Creation
+    $PartitionSchemes = $db.PartitionSchemes
+    if ($PartitionSchemes.Count -gt 0) {
+        #Scripting Options
+        $options.IncludeDatabaseContext  = $true
+
+
+        # Create Directory for Scripts
+        $currentPath = $DirDatabase + '\PartitionSchemes'
+        if(!(Test-Path -Path $currentPath)){
+            # Create Directory for Database
+            New-Item -ItemType directory -Path $currentPath
+        }
+
+        # Generate Scripts
+        foreach($PartitionScheme in $PartitionSchemes) {
+            $FileName = $currentPath + '\' + $PartitionScheme.Schema +'.' + $PartitionScheme.Name + '.sql'
+            Export-DbaScript -InputObject $PartitionScheme -Path $FileName -ScriptingOptionsObject $options -NoPrefix
+        }
+    }
+    #endregion
+
+    #region Sequences Creation
+    $Sequences = $db.Sequences
+    if ($Sequences.Count -gt 0) {
+        #Scripting Options
+        $options.IncludeDatabaseContext  = $true
+
+        # Create Directory for Scripts
+        $currentPath = $DirDatabase + '\Sequences'
+        if(!(Test-Path -Path $currentPath)){
+            # Create Directory for Database
+            New-Item -ItemType directory -Path $currentPath
+        }
+
+        # Generate Scripts
+        foreach($Sequence in $Sequences) {
+            $FileName = $currentPath + '\' + $Sequence.Schema +'.' + $Sequence.Name + '.sql'
+            Export-DbaScript -InputObject $Sequence -Path $FileName -ScriptingOptionsObject $options -NoPrefix
+        }
+    }
+    #endregion
+
+
+    #region Assemblies Creation
+    $Assemblies = ($db.Assemblies | Where-Object {$_.IsSystemObject -eq $false})
+    if ($Assemblies.Count -gt 0) {
+        #Scripting Options
+        $options.IncludeDatabaseContext  = $true
+
+        # Create Directory for Scripts
+        $currentPath = $DirDatabase + '\Assemblies'
+        if(!(Test-Path -Path $currentPath)){
+            # Create Directory for Database
+            New-Item -ItemType directory -Path $currentPath
+        }
+
+        # Generate Scripts
+        foreach($Assembly in $Assemblies) {
+            $FileName = $currentPath + '\' + $Assembly.Schema +'.' + $Assembly.Name + '.sql'
+            Export-DbaScript -InputObject $Assembly -Path $FileName -ScriptingOptionsObject $options -NoPrefix
+        }
+    }
+    #endregion
+
+    #region Roles Creation
+    $Roles = ($db.Roles | Where-Object {($_.IsFixedRole -eq $false -and $_.Name -ne 'public')})
+    if ($Roles.Count -gt 0) {
+        #Scripting Options
+        $options.IncludeDatabaseContext  = $true
+        $Options.IncludeIfNotExists = $true
+
+        # Create Directory for Scripts
+        $currentPath = $DirDatabase + '\Roles'
+        if(!(Test-Path -Path $currentPath)){
+            # Create Directory for Database
+            New-Item -ItemType directory -Path $currentPath
+        }
+
+        # Generate Scripts
+        foreach($Role in $Roles) {
+            $FileName = $currentPath + '\' + $Role.Name + '.sql'
+            Export-DbaScript -InputObject $Role -Path $FileName -ScriptingOptionsObject $options -NoPrefix
+        }
+    }
+    #endregion
+
+    #region Synonyms Creation
+    $Synonyms = $db.Synonyms
+    if ($Synonyms.Count -gt 0) {
+        #Scripting Options
+        $options.IncludeDatabaseContext  = $true
+
+        # Create Directory for Scripts
+        $currentPath = $DirDatabase + '\Synonyms'
+        if(!(Test-Path -Path $currentPath)){
+            # Create Directory for Database
+            New-Item -ItemType directory -Path $currentPath
+        }
+
+        # Generate Scripts
+        foreach($Synonym in $Synonyms) {
+            $FileName = $currentPath + '\' + $Synonym.Schema +'.' + $Synonym.Name + '.sql'
+            Export-DbaScript -InputObject $Synonym -Path $FileName -ScriptingOptionsObject $options -NoPrefix
+        }
+    }
+    #endregion
+
+    #region UserDefinedAggregates Creation
+    $UserDefinedAggregates = $db.UserDefinedAggregates
+    if ($UserDefinedAggregates.Count -gt 0) {
+        #Scripting Options
+        $options.IncludeDatabaseContext  = $true
+
+
+        # Create Directory for Scripts
+        $currentPath = $DirDatabase + '\UserDefinedAggregates'
+        if(!(Test-Path -Path $currentPath)){
+            # Create Directory for Database
+            New-Item -ItemType directory -Path $currentPath
+        }
+
+        # Generate Scripts
+        foreach($uda in $UserDefinedAggregates) {
+            $FileName = $currentPath + '\' + $uda.Schema +'.' + $uda.Name + '.sql'
+            Export-DbaScript -InputObject $uda -Path $FileName -ScriptingOptionsObject $options -NoPrefix
+        }
+    }
+    #endregion
+
+    #region UserDefinedTypes Creation
+    $UserDefinedTypes = $db.UserDefinedTypes
+    if ($UserDefinedTypes.Count -gt 0) {
+        #Scripting Options
+        $options.IncludeDatabaseContext  = $true
+
+
+        # Create Directory for Scripts
+        $currentPath = $DirDatabase + '\UserDefinedTypes'
+        if(!(Test-Path -Path $currentPath)){
+            # Create Directory for Database
+            New-Item -ItemType directory -Path $currentPath
+        }
+
+        # Generate Scripts
+        foreach($udt in $UserDefinedTypes) {
+            $FileName = $currentPath + '\' + $udt.Schema +'.' + $udt.Name + '.sql'
+            Export-DbaScript -InputObject $udt -Path $FileName -ScriptingOptionsObject $options -NoPrefix
+        }
+    }
+    #endregion
+
+    #region Users Creation
+    $Users = ($db.Users | Where-Object {$_.IsSystemObject -eq $false})
+    if ($Users.Count -gt 0) {
+        #Scripting Options
+        $options.IncludeDatabaseContext  = $true
+
+
+        # Create Directory for Scripts
+        $currentPath = $DirDatabase + '\Users'
+        if(!(Test-Path -Path $currentPath)){
+            # Create Directory for Database
+            New-Item -ItemType directory -Path $currentPath
+        }
+
+        # Generate Scripts
+        foreach($User in $Users) {
+            $UserName = $User.Name -replace '[\\\/\:\.]','$'
+            $FileName = $currentPath + '\' + $UserName + '.sql'
+
+            Export-DbaScript -InputObject $User -Path $FileName -ScriptingOptionsObject $options -NoPrefix
+        }
+    }
+    #endregion
+
+    # Add files to git repository 
+    set-location $BasePath
+    if(!(Test-Path -Path $BasePath\.git)){
+        git init    
+    } 
+    else {
+        git status
+    }
+
+    git add .
+    git commit -am "Add all Files to Git Repository"
+
+
 }
-
-
-
 
