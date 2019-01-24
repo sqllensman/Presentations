f0382f3fbece67114229cfe9895be4b1a7319a1d
Patrick Flynn <sqllensman@outlook.com>
2019-01-25 00:19:14 +1100

    Commiting Presentation updates

---
+++ SQLSaturday Malaysia 818/SyncPresentation.ps1
@@ -0,0 +1,12 @@
+﻿set-location "C:\GitHub\Presentations\SQLSaturday Malaysia 818"
+# Stage files
+git add *
+# Commit files locally
+git commit -m "Commiting Presentation" 
+
+# View Status
+git status
+
+git remote -v
+
+git push -u origin
