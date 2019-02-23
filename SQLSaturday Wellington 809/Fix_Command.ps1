96e0fd713a755471665a9c12a366483f5d5445c1
Patrick Flynn <sqllensman@outlook.com>
2019-02-23 11:11:28 +1100

    Commiting Presentation Files

---
+++ SQLSaturday Wellington 809/Fix_Command.ps1
@@ -0,0 +1,9 @@
+﻿Remove-Module -Name dbatools
+# Load local copy
+Import-Module "C:\GitHub\dbatools\dbatools.psm1" -Force
+
+Get-Module -Name dbatools
+
+$SQLinstance = 'LensmanSB'
+
+Invoke-DbaDbShrink -SqlInstance $SQLinstance -AllUserDatabases -WhatIf
